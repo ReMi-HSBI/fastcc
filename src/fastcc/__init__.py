94aff1fa1ec3b2432c6bf0d1ecf6e8cd424547ee
Justin Baudisch <justin.baudisch@hsbi.de>
2025-01-30 13:21:49 +0100

    :construction: Add fundamentals

--- src/fastcc/__init__.py
+++ src/fastcc/__init__.py
@@ -13,3 +13,137 @@ References
 .. [1] https://mqtt.org
 .. [2] https://protobuf.dev/
 """
+
+from __future__ import annotations
+
+import logging
+import typing
+
+if typing.TYPE_CHECKING:
+    from collections.abc import Callable
+
+    from fastcc.utilities.type_aliases import Routable
+
+import aiomqtt
+
+from fastcc.utilities.mqtt import QoS
+
+__all__ = ["CCRouter", "FastCC"]
+
+_logger = logging.getLogger(__name__)
+
+
+class CCRouter:
+    """Router for communication endpoints."""
+
+    def __init__(self) -> None:
+        self._routes: dict[str, dict[int, list[Routable]]] = {}
+
+    @property
+    def routes(self) -> dict[str, dict[int, list[Routable]]]:
+        """Return all registered routes.
+
+        Returns
+        -------
+        dict[str, dict[int, list[Routable]]]
+            All registered routes.
+        """
+        return self._routes
+
+    def route(
+        self,
+        topic: str,
+        *,
+        qos: QoS = QoS.AT_MOST_ONCE,
+    ) -> Callable[[Routable], Routable]:
+        """Register a (MQTT-) route.
+
+        Parameters
+        ----------
+        topic
+            MQTT topic to subscribe and assign the function to.
+        qos
+            Quality of Service level for the subscription.
+
+        Returns
+        -------
+        Callable[[Routable], Routable]
+            Decorated callable.
+        """
+
+        def decorator(routable: Routable) -> Routable:
+            if topic not in self._routes:
+                self._routes[topic] = {}
+
+            if qos not in self._routes[topic]:
+                self._routes[topic][qos] = []
+
+            self._routes[topic][qos].append(routable)
+
+            return routable
+
+        return decorator
+
+    def add_router(self, router: CCRouter) -> None:
+        """Add another router to this router.
+
+        Parameters
+        ----------
+        router
+            Router to add.
+        """
+        self._routes.update(router.routes)
+
+
+class FastCC:
+    """Application class of FastCC.
+
+    Parameters
+    ----------
+    client
+        MQTT client instance.
+
+    Raises
+    ------
+    ValueError
+        If `client` does not support MQTT version 5.0.
+    """
+
+    def __init__(self, client: aiomqtt.Client) -> None:
+        if client._client.protocol != aiomqtt.ProtocolVersion.V5:  # noqa: SLF001
+            details = "client must support MQTT version 5.0"
+            _logger.error(details)
+            raise ValueError(details)
+
+        self._client = client
+        self._router = CCRouter()
+
+    async def run(self) -> None:
+        """Start the application.
+
+        Raises
+        ------
+        ValueError
+            If the initialized MQTT client is not connected.
+        """
+        if not self._client._connected.done():  # noqa: SLF001
+            details = "client is not connected"
+            _logger.error(details)
+            raise ValueError(details)
+
+        for topic, data in self._router.routes.items():
+            for qos in data:
+                _logger.info("Subscribing to topic %s (QoS %d)", topic, qos)
+                await self._client.subscribe(topic, qos)
+
+        _logger.info("Hello, FastCC!")
+
+    def add_router(self, router: CCRouter) -> None:
+        """Add a router to the app.
+
+        Parameters
+        ----------
+        router
+            Router to add.
+        """
+        self._router.add_router(router)
