1ab7cf99c640795fd4b861b35cd230242e397a03
Justin Baudisch <justin.baudisch@hsbi.de>
2026-02-26 12:57:53 +0100

    :wrench: add project

---
+++ src/fastcc/__init__.py
@@ -0,0 +1,23 @@
+"""Framework for MQTT communication.
+
+FastCC is a lightweight, efficient and developer-friendly framework for
+MQTT [1]_ communication. It is built on top of the **aiomqtt** [2]_
+library and extends it with the following functionalities:
+
++----------------------------------+-----------+
+| Feature                          | Status    |
++==================================+===========+
+| Routing                          | 📋Planned |
++----------------------------------+-----------+
+| Custom payload encoding/decoding | 📋Planned |
++----------------------------------+-----------+
+| Request/Response                 | 📋Planned |
++----------------------------------+-----------+
+| Streaming                        | 📋Planned |
++----------------------------------+-----------+
+
+References
+----------
+.. [1] https://mqtt.org
+.. [2] https://github.com/empicano/aiomqtt
+"""
